68858452411653ebecda9d4992ceab516d916021
JoeCalvi <joecalvi@me.com>
2023-06-24 14:44:03 -0600

    player can move vertically

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput;
     public float speed = 10.0f;
     public float xRange = 14.0f;
 
+    public float lowBound = 0;
+    public float highBound = 5.0f;
     public GameObject projectilePrefab;
 
     // Start is called before the first frame update
@@ -29,10 +32,21 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        if(transform.position.z < lowBound) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, lowBound);
+        }
+
+        if(transform.position.z > highBound) {
+            transform.position = new Vector3(transform.position.x, transform.position.y, highBound);
+        }
         // SECTION end
 
          horizontalInput = Input.GetAxis("Horizontal");
          transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+         verticalInput = Input.GetAxis("Vertical");
+         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         // how to make something happen when Spacebar is pressed
          if(Input.GetKeyDown(KeyCode.Space))
